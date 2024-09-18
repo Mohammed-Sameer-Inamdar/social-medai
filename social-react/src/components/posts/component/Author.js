c38821147dabf9269238472e30b450396cd9f169
Mohammed Sameer <mohammedsameerinamdar@gmail.com>
2024-09-18 16:12:18 +0530

    removed react project

--- social-react/src/components/posts/component/Author.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Author = ({ author }) => {
-    return (
-        <span style={{ textTransform: 'capitalize' }}>by {author?.username ?? 'Unknow Author'}</span>
-    )
-}
-
-export default Author
